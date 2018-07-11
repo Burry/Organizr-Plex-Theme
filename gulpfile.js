5d0681c6b0f54e330f26ef9f11df9b79fea91072
Grant Burry <grant@burry.me>
2018-07-10 21:59:34 -0600

    Version 1.7.0 — Finer details, dev tools, & docs
    - Fixed homepage button style (symbols only)
    - Fixed calendar details
    - Fixed lots of other style details
    - Added Browsersync for real-time dev testing
    - Better documentation, including js fix for iframe topOffset

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,41 @@
 'use strict';
 
-let gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
     sassLint = require('gulp-sass-lint'),
     cleanCSS = require('gulp-clean-css'),
     packageJSON = require('./package.json'),
     autoPrefixer = require('gulp-autoprefixer'),
-    headerComment = require('gulp-header-comment');
-
-// Build Sass files when changes detected
-gulp.task('watch', function() {
-    gulp.watch("scss/**/*.s+(a|c)ss", ['build']);
-});
+    headerComment = require('gulp-header-comment'),
+    browserSync = require('browser-sync').create(),
+    browserSyncConfig = theme => ({
+        proxy: packageJSON.homepage,
+        files: 'css/' + theme + '.css',
+        serveStatic: ['css'],
+        snippetOptions: {
+            rule: {
+                match: /<\/head>/i,
+                fn: (snippet, match) => '<link rel="stylesheet" type="text/css" href="/' + theme + '.css"/>' + snippet + match
+            }
+        },
+        ghostMode: {
+            clicks: false,
+            location: false,
+            forms: false,
+            scroll: false
+        },
+        notify: {
+            styles: {
+        		top: 'auto',
+        		bottom: '0'
+            }
+        }
+    });
 
 // Lint, compile, and post-process Sass
-gulp.task('build', function() {
+gulp.task('compile', function() {
     return gulp
-        .src("scss/*.s+(a|c)ss")
+        .src('scss/*.s+(a|c)ss')
         .pipe(sassLint({ options: { 'merge-default-rules': false } }))
         .pipe(sassLint.format())
         .pipe(sass({ errLogToConsole: true }).on('error', sass.logError))
@@ -27,7 +46,43 @@ gulp.task('build', function() {
             Version ` + packageJSON.version + `
             ` + packageJSON.license + ` License
         ` + packageJSON.repository))
-        .pipe(gulp.dest("css"));
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['build', 'watch']);
+// Build Sass files when changes detected
+gulp.task('watch', function() {
+    return gulp.watch('scss/**/*.s+(a|c)ss', gulp.series('build'));
+});
+
+gulp.task('build', gulp.series(['compile', 'watch']));
+
+// Start BrowserSync server
+function startBrowserSync(theme) {
+    if (!packageJSON.homepage) {
+        console.error('Set the "homepage" value in package.json to your Organizr instance\'s URL to test with Browsersync');
+        return;
+    }
+    return browserSync.init(browserSyncConfig(theme));
+}
+
+gulp.task('serve-plex-theme', function() {
+    return startBrowserSync('Plex');
+});
+
+gulp.task('serve-plex-blur-theme', function() {
+    return startBrowserSync('Plex Blur');
+});
+
+gulp.task('serve-plex-tv-theme', function() {
+    return startBrowserSync('Plex.tv');
+});
+
+gulp.task('serve-plex-tv-blur-theme', function() {
+    return startBrowserSync('Plex.tv Blur');
+});
+
+gulp.task('build-serve-plex-theme', gulp.parallel('build', 'serve-plex-theme'));
+gulp.task('build-serve-plex-blur-theme', gulp.parallel('build', 'serve-plex-blur-theme'));
+gulp.task('build-serve-plex-tv-theme', gulp.parallel('build', 'serve-plex-tv-theme'));
+gulp.task('build-serve-plex-tv-blur-theme', gulp.parallel('build', 'serve-plex-tv-blur-theme'));

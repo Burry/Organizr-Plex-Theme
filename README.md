bdc01986907703a906d2c37064fb392cdc223d05
Grant Burry <grant@burry.me>
2018-07-15 19:05:46 -0600

    No longer need to install images, + minor fixes

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ A theme for Organizr v2 that emulates the style of Plex
 - Open Organizr Settings > Customize > Appearance > Colors & Themes, and select "Plex" from the theme dropdown input.
 - Open Organizr's `/js/custom.min.js`, search for the string `h=40`, and change it to `h=60` so that `iframe`s compensate for Plex Theme's increased nav bar padding.
 - To ensure that notifications display as intended, keep the default notification style setting "Izi" under Organizr Settings > Customize > Appearance > Notifications.
-- *Optional*: Copy the contents of [Plex Theme's `/images`](https://github.com/Burry/organizr-v2-plex-theme/tree/master/images) to Organizr's `/plugins/images/` directory. Plex Theme links to some assets hosted on its remote repository, but assets such as group icons are also provided which must be installed directly.
 
 ### Hide Login Page UI Chrome
 
@@ -25,16 +24,24 @@ Add the following lines to Organizr's `/js/functions.js` inside the function `sw
 
 Changing the placeholder color values in Organizr's Colors & Themes will override Plex Theme's color styles. Deleting custom color values will reverse this. Overrides can also be made in Organizr's Custom CSS settings pane.
 
+### Group Icons
+
+Some group icons are provided for your use in [`/images/groups`](https://github.com/Burry/organizr-v2-plex-theme/tree/master/images/groups), to be installed in Organizr's `/plugins/images/groups`.
+
 ## Development
 
 - Install [Yarn](https://yarnpkg.com/en/docs/install)
 - [Download](https://github.com/Burry/organizr-v2-plex-theme/archive/master.zip) or clone the repository and enter it in a terminal
 - Run `yarn` to install development dependencies
-- Set the `homepage` value in `package.json` to your Organizr instance's URL
-- Run `yarn serve` to...
-    - Compile the [Sass](https://sass-lang.com/documentation/file.SASS_REFERENCE.html) source stylesheets to a minified CSS file and recompile when changes are detected
-    - Launch a new [Browsersync](https://browsersync.io) window with the Plex Theme css injected into your Organizr instance
 
-As you make changes in `/scss`, the stylesheet will be continually recompiled and automatically injected into the Browsersync window, so you can see your changes instantly.
+#### `yarn build`
 
-You can also just run `yarn build` to compile the stylesheets and watch for changes without serving a test window.
+Compiles the [Sass](https://sass-lang.com/documentation/file.SASS_REFERENCE.html) source stylesheets in `/scss` into a minified CSS file and recompiles when changes are detected until exited.
+
+#### `yarn serve`
+
+You must first set the `homepage` value in `package.json` to your Organizr instance's URL. Then `yarn serve` runs `yarn build` and launches a new [Browsersync](https://browsersync.io) window with the Plex Theme css injected into your Organizr instance every time it is recompiled, so you can see your changes instantly.
+
+#### `yarn imagemin`
+
+Losslessly optimizes all `.svg`, `.png`, `.jpg`, and `.gif` images in the repository with [imagemin](https://github.com/imagemin/imagemin). This will take some time.
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 'use strict';
 
 const gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
     sass = require('gulp-sass'),
     sassLint = require('gulp-sass-lint'),
+    sassInlineImage = require('sass-inline-image'),
+    autoPrefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     packageJSON = require('./package.json'),
-    autoPrefixer = require('gulp-autoprefixer'),
     headerComment = require('gulp-header-comment'),
-    comment = `Plex Theme for Organizr v2
-        Version ` + packageJSON.version + `
-        ` + packageJSON.license + ` License
-        ` + packageJSON.repository,
     browserSync = require('browser-sync').create(),
     browserSnippet = '<link rel="stylesheet" type="text/css" href="/Plex.css">',
+    browserSyncError = 'Set the "homepage" value in package.json to your Organizr instance\'s URL to test with Browsersync',
     browserSyncConfig = {
         proxy: packageJSON.homepage,
         files: 'css/Plex.css',
@@ -23,42 +22,81 @@ const gulp = require('gulp'),
                 fn: (snippet, match) => browserSnippet + snippet
             }
         },
+        ghostMode: {
+            clicks: false,
+            location: false,
+            forms: false,
+            scroll: false
+        },
         notify: {
             styles: {
         		top: 'auto',
         		bottom: '0'
             }
         }
-    };
+    },
+    imageminPlugins = [
+        imagemin.svgo(),
+        imagemin.optipng({ optimizationLevel: 7 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.gifsicle({
+            interlaced: true,
+            optimizationLevel: 3
+        })
+    ],
+    imagesGlob = '**/*.+(svg|png|jpg|jpeg|gif)',
+    comment = `Plex Theme for Organizr v2
+        Version ` + packageJSON.version + `
+        ` + packageJSON.license + ` License
+        ` + packageJSON.repository;
+
+
+// Losslessly optimize images
+gulp.task('imagemin', () =>
+    gulp
+        .src(imagesGlob)
+        .pipe(imagemin(imageminPlugins))
+        .pipe(gulp.dest('./'))
+);
+
 
 // Lint, compile, and post-process Sass
-gulp.task('compile', function() {
-    return gulp
+gulp.task('compile', () =>
+    gulp
         .src('scss/*.s+(a|c)ss')
-        .pipe(sassLint({ options: { 'merge-default-rules': false } }))
+        .pipe(sassLint({
+            options: { 'merge-default-rules': false }
+        }))
         .pipe(sassLint.format())
-        .pipe(sass({ errLogToConsole: true }).on('error', sass.logError))
+        .pipe(sass({
+            errLogToConsole: true,
+            functions: sassInlineImage()
+        }).on('error', sass.logError))
         .pipe(autoPrefixer())
         .pipe(cleanCSS({ level: 2 }))
         .pipe(headerComment(comment))
         .pipe(gulp.dest('css'))
-        .pipe(browserSync.stream());
-});
+        .pipe(browserSync.stream())
+);
+
 
 // Build Sass files when changes detected
-gulp.task('watch', function() {
-    return gulp.watch('scss/**/*.s+(a|c)ss', gulp.series('build'));
-});
+gulp.task('watch', () =>
+    gulp.watch([imagesGlob, 'scss/**/*.s+(a|c)ss'], gulp.series('compile'))
+);
 
 gulp.task('build', gulp.series(['compile', 'watch']));
 
+
 // Start BrowserSync server
-gulp.task('serve', function() {
-    if (!packageJSON.homepage) {
-        console.error('Set the "homepage" value in package.json to your Organizr instance\'s URL to test with Browsersync');
-        return process.exit(0);
-    }
-    return browserSync.init(browserSyncConfig);
-});
+const serveError = () => {
+    console.error(browserSyncError);
+    return process.exit(0);
+};
+
+gulp.task('serve', () => !packageJSON.homepage
+    ? serveError
+    : browserSync.init(browserSyncConfig)
+);
 
 gulp.task('build-serve', gulp.parallel('build', 'serve'));
